daec1773e91450430fc8dc39ab06e9eb3b8e4d15
Polina Lappo <polina.lappo@soft-werke.com>
2019-03-28 23:59:11 +0300

    setting webpack & add class Developer

---
+++ src/Developer.js
@@ -0,0 +1,8 @@
+class Developer {
+    constructor(name="polina", language="javascript") { 
+        this.name = name;
+        this.language = language;
+    }
+}
+
+export default Developer;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path'),
+    HTMLplugin = require('html-webpack-plugin'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+    UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'img/[hash].[ext]'
+            }
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HTMLplugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+};
