994aa5988586e4b45444580ae05f20bd51914829
Polina Lappo <polina.lappo@soft-werke.com>
2019-04-02 01:27:48 +0300

    add title for component

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,10 @@
-    
 import React from 'react';
 
 class Login extends React.Component {
   render() {
     return (
         <form name='loginForm'>
+        <h2>{this.props.titleLogin}</h2>
             <input name='name' type='text'/>
             <br/>
             <input name='password' type='password'/>
