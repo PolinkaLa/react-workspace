d7a028e36be4138d9b77fcea8551bd9b8309a66e
Polina Lappo <polina.lappo@soft-werke.com>
2019-04-05 01:23:10 +0300

    change get props & add state for show/hide article

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
 class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {show: false}
+  }
+  handleClick(){
+    console.log(this.state.show)
+    this.state.show ? this.setState({show: false}): this.setState({show: true});
+  }
   render() {
-    return (
+    const {title, postBody, date, author} = this.props.item
+    const isShow = this.state.show;
+    if (isShow){
+      return (
         <article className="jumbotron">
-            <h2>{this.props.title}</h2>
-            <p>{this.props.postBody}</p>
+            <h2 onClick={this.handleClick}>{title} <i><span className="badge badge-info">hide</span></i></h2>
+            <p>{postBody}</p>
+            <p><i>Created </i><span className="badge badge-secondary">{date}</span></p>
+            <p><i>Author </i><span className="badge badge-primary">{author}</span></p>
         </article>
     );
+    }
+    else{
+      return (
+        <article className="jumbotron">
+            <h2 onClick={this.handleClick}>{title} <i><span className="badge badge-info">show</span></i></h2>
+        </article>
+    );
+    }
+    
     }
   }
 
