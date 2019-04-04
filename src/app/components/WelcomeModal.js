0aa4f89476c7e9145cc127f3734646537c157ca0
Polina Lappo <polina.lappo@soft-werke.com>
2019-04-05 00:45:44 +0300

    releasation of modal

--- src/app/components/WelcomeModal.js
+++ src/app/components/WelcomeModal.js
@@ -1,25 +1,46 @@
 import React from 'react';
 
 class WelcomeModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.closeModal = this.closeModal.bind(this);
+    this.state = {modal: false}
+  }
+  closeModal(){
+    this.setState({modal: false});
+  }
+  componentDidMount(){
+    console.log("componentDidMount()");
+    this.setState({modal: true});
+  }
   render() {
-    return (
-      <div classNameName="modal" tabindex="-1" role="dialog">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Modal title</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <p>Modal body text goes here.</p>
+    const isShow = this.state.modal;
+
+    if (isShow) {
+      return (
+        <React.Fragment>
+          <div className="modal fade show" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{display:'block'}}>
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Welcome!</h5>
+                  <button onClick={this.closeModal} type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <p>Welcome</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    );
+        </React.Fragment>
+      );
+    }
+    else {
+      return <React.Fragment/>
     }
   }
+}
 
 export default WelcomeModal;
