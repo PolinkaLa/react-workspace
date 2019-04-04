79fe36b6415d30d91ee958087b4e8b0f196f66e1
plappo <polina.lappo@soft-werke.com>
2019-04-04 19:51:38 +0300

    render posts

--- src/app/components/Blog.js
+++ src/app/components/Blog.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Post from './Post';
 
 class Blog extends React.Component {
   render() {
-    return;
+    const postsData = this.props.postsData.map((item, index) => {
+        return <Post key={index} href={item.title}>{item.postBody}</Post>
+      })
+    return (
+        <Fragment>
+          {postsData}
+        </Fragment>
+    );
     }
   }
 
