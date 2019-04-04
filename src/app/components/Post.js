557ee4f1b87e5b6c3560926158f26218ca976f2b
Polina Lappo <polina.lappo@soft-werke.com>
2019-04-04 23:01:03 +0300

    apply bootstrap

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Post extends React.Component {
   render() {
     return (
-        <article>
+        <article class="jumbotron">
             <h2>{this.props.title}</h2>
             <p>{this.props.postBody}</p>
         </article>
