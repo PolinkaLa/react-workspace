5864c6de92db4f814d6474b2e8af7ee3dbcab1d8
Polina Lappo <polina.lappo@soft-werke.com>
2019-04-07 22:23:51 +0300

    add new path to routing

--- src/app.js
+++ src/app.js
@@ -8,20 +8,24 @@ import MainPage from "./app/pages/Main";
 import PageNotFound from "./app/pages/PageNotFound";
 import Users from "./app/pages/Users";
 import User from "./app/pages/User";
-
+import Post from "./app/pages/Post";
+import Posts from "./app/pages/Posts";
+import Comment from "./app/pages/Comment";
+import Comments from "./app/pages/Comments";
 
 ReactDOM.render(
 	<Router history={browserHistory}>
 		<Route path="/" component={Layout}>
 			<IndexRoute component={MainPage}/>
+			<Route path="posts" component={Posts}>
+				<Route path=":postId" component={Post}/>
+			</Route>
+			<Route path="comments" component={Comments}>
+				<Route path=":commentId" component={Comment}/>
+			</Route>
 			<Route path="users" component={Users}>
 				<Route path=":userId" component={User}/>
 			</Route>
 			<Route path="*" component={PageNotFound}/>
 		</Route>
 	</Router>, document.getElementById("root"));
-
-
-	// <Layout>
-	// 	<About/>
-	// </Layout>
